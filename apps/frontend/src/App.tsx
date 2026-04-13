580caff3364dc23289a4119075740de671e9c926
Youlim Jayde Jung <yljung.jay@gmail.com>
2026-04-13 15:56:00 +0900

    removed: Reliability Views from NavBar and /reliability-views route modified: useSimulation to stay on BayesianPage after submit

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 
 import BayesianPage from './features/bayesian/components/BayesianPage';
 import StatisticalPage from "./features/statistical/components/StatisticalPage";
-import ReliabilityPage from "./features/reliability/components/ReliabilityPage";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Route path="/" element={ <BayesianPage /> } />
       <Route path="/bayesian" element={ <BayesianPage /> } />
       <Route path="/statistical" element={ <StatisticalPage/> } />
-      <Route path="/reliability-views/:jobId?" element={ <ReliabilityPage/> } />
     </Routes>
   );
 }
